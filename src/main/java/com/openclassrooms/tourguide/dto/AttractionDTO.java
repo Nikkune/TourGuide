510d6c6c5cdf2f1e30d32042beff5c6a862891a9
Nikkune <dev.2tvhg@passmail.net>
2025-08-04 10:20:54 +0200

    Add `AttractionDTO` to encapsulate attraction details for improved data transfer.

---
+++ src/main/java/com/openclassrooms/tourguide/dto/AttractionDTO.java
@@ -0,0 +1,86 @@
+package com.openclassrooms.tourguide.dto;
+
+/**
+ * Data Transfer Object for attraction information
+ * Contains information about an attraction, its location, the user's location,
+ * the distance between them, and the reward points for visiting the attraction.
+ */
+public class AttractionDTO {
+    private String attractionName;
+    private double attractionLatitude;
+    private double attractionLongitude;
+    private double userLatitude;
+    private double userLongitude;
+    private double distance;
+    private int rewardPoints;
+
+    public AttractionDTO() {
+    }
+
+    public AttractionDTO(String attractionName, double attractionLatitude, double attractionLongitude,
+                         double userLatitude, double userLongitude, double distance, int rewardPoints) {
+        this.attractionName = attractionName;
+        this.attractionLatitude = attractionLatitude;
+        this.attractionLongitude = attractionLongitude;
+        this.userLatitude = userLatitude;
+        this.userLongitude = userLongitude;
+        this.distance = distance;
+        this.rewardPoints = rewardPoints;
+    }
+
+    public String getAttractionName() {
+        return attractionName;
+    }
+
+    public void setAttractionName(String attractionName) {
+        this.attractionName = attractionName;
+    }
+
+    public double getAttractionLatitude() {
+        return attractionLatitude;
+    }
+
+    public void setAttractionLatitude(double attractionLatitude) {
+        this.attractionLatitude = attractionLatitude;
+    }
+
+    public double getAttractionLongitude() {
+        return attractionLongitude;
+    }
+
+    public void setAttractionLongitude(double attractionLongitude) {
+        this.attractionLongitude = attractionLongitude;
+    }
+
+    public double getUserLatitude() {
+        return userLatitude;
+    }
+
+    public void setUserLatitude(double userLatitude) {
+        this.userLatitude = userLatitude;
+    }
+
+    public double getUserLongitude() {
+        return userLongitude;
+    }
+
+    public void setUserLongitude(double userLongitude) {
+        this.userLongitude = userLongitude;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public int getRewardPoints() {
+        return rewardPoints;
+    }
+
+    public void setRewardPoints(int rewardPoints) {
+        this.rewardPoints = rewardPoints;
+    }
+}
